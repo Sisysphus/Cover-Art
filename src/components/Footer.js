956aa53889014b97b84682617ec6639641269c69
Adam Faidy <faidyadam1@gmail.com>
2021-06-19 22:13:43 -0400

    Basic footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Footer() {
-  return <div></div>;
+  return (
+    <div className="footer-container">
+      <div className="footer-ui">
+        <li>LinkedIn</li>
+        <li>Github</li>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
