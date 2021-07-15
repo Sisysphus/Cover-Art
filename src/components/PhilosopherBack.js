bc7ea58fe60b7e45fc91dcc9c476c3896f1b1cfd
Adam Faidy <faidyadam1@gmail.com>
2021-07-15 13:38:31 -0400

    Philo background - component structure ( Pre-CSS " "

---
+++ src/components/PhilosopherBack.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function PhilosopherBack() {
+  return (
+    <div className="philo-bio">
+      <div className="philo-bio-wrap">
+        <div className="philo-bio-container"></div>
+      </div>
+    </div>
+  );
+}
+
+export default PhilosopherBack;
