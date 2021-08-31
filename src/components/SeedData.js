fb23d23cb4e96a9f4b39b1c96a7046f06143cc03
Adam Faidy <Faidyadam1@gmail.com>
2021-08-30 23:59:23 -0400

    cover art

--- src/components/SeedData.js
+++ src/components/SeedData.js
@@ -11,13 +11,26 @@ export const PhilosopherInfo = [
   },
   {
     wikilink: "https://en.wikipedia.org/wiki/Aristotle",
-    date: "Born in 384 BCE",
+    date: "Born in 412 BCE",
+    description: `was a Greek philosopher and polymath during the Classical period in Ancient Greece. Taught by Plato, he was the founder of the Lyceum, the Peripatetic school of philosophy, and the Aristotelian tradition. His writings cover many subjects including physics, biology, zoology, metaphysics, logic, ethics, aesthetics, poetry, theatre, music, rhetoric, psychology, linguistics, economics, politics, meteorology, geology and government. Aristotle provided a complex synthesis of the various philosophies existing prior to him.`,
+    img: "/images/aristotle.jpg",
+    name: "Aristotle",
+  },
+  {
+    wikilink: "https://en.wikipedia.org/wiki/Diogenes",
+    date: "Born in 532 BCE",
+    description: `was a Greek philosopher and polymath during the Classical period in Ancient Greece. Taught by Plato, he was the founder of the Lyceum, the Peripatetic school of philosophy, and the Aristotelian tradition. His writings cover many subjects including physics, biology, zoology, metaphysics, logic, ethics, aesthetics, poetry, theatre, music, rhetoric, psychology, linguistics, economics, politics, meteorology, geology and government. Aristotle provided a complex synthesis of the various philosophies existing prior to him.`,
+    img: "/images/Socrates.jpg",
+    name: "Socrates",
+  },
+  {
+    wikilink: "https://en.wikipedia.org/wiki/Socrates",
+    date: "Born in 412 BCE",
     description: `was a Greek philosopher and polymath during the Classical period in Ancient Greece. Taught by Plato, he was the founder of the Lyceum, the Peripatetic school of philosophy, and the Aristotelian tradition. His writings cover many subjects including physics, biology, zoology, metaphysics, logic, ethics, aesthetics, poetry, theatre, music, rhetoric, psychology, linguistics, economics, politics, meteorology, geology and government. Aristotle provided a complex synthesis of the various philosophies existing prior to him.`,
     img: "/images/aristotle.jpg",
     name: "Aristotle",
   },
 ];
-
 
 // export const Articles = [
 //     src=
